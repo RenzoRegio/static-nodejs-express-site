29b0f64a02d8d7b66d2398151f9b345ebf9c22c3
RenzoRegio <renzorregio@gmail.com>
2021-01-19 17:41:26 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # static-nodejs-express-site
- 
+
+Added CSS Styles:
+
+1. All h1 elements
+
+   - Font-weight was changed to 800 to add boldness to the text.
+   - Added a link to layout.pug to reference the "Yusei Magic" font-family retrieved from google fonts.
+   - Changed all h1 elements to have the color white.
+   - Added a text-shadow effect.
+
+2. Thumbnail images on the index.pug template which shows the project previews/cover images for each project.
+
+   - Hover effect to scale the image.
+   - Box-shadow effect.
+   - Border and border-radius.
+   - Transition.
+
+3. Side-bar style (the content extended from layout.pug and is shown on all pages)
+
+   - Added a background-image: linear-gradient color to match the main background color/style of all images.
+   - Border-right.
+
+4. Main content background style
+
+   - Added keyframes to present a gradient background effect on the content's background color/style - shown on all pages.
+
+5. Error pages - CSS style
+
+   - CSS Styles relating to all elements contained on the error and page-not-found templates
+
+6. About page - programming languages/frameworks container
+   - Added a couple of HTML elements to add this feature to present some of the languages or frameworks I am currently working with or worked with in the past. This feature does not alter the initial layout or position of the important elements of the page but extends it.
+   - Multiple effects / styles added to this section.
--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const data = require("./data.json");
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
 
+/* GET projects - render index pug template passing the projects array */
+
 app.get("/", (req, res) => {
   res.locals.projects = data.projects;
   res.render("index");
 });
 
-app.get("/about", (req, res) => {
+/* GET - renders the about pug template containing my profile information and other relevant data about my profile details */
+
+app.get("/about", (req, res, next) => {
   res.render("about");
 });
+
+/* GET individual project route - renders an individual project page containing the project selected by a user */
 
 app.get("/projects/:id", (req, res) => {
   const { id } = req.params;
@@ -20,20 +26,28 @@ app.get("/projects/:id", (req, res) => {
   res.render("project", { project });
 });
 
-app.use((req, rest, next) => {
+/* ERROR HANDLERS */
+
+/* 404 Error Handler that catches undefined or non-existent routes */
+
+app.use((req, res, next) => {
   const err = new Error("Page not Found");
   err.statusCode = 404;
-  console.log(`${err.message}:`, err.statusCode);
   next(err);
 });
 
+/* Global Error Handler */
+
 app.use((err, req, res, next) => {
   if (err.statusCode === 404) {
+    // If the error's status code is 404 (retrieved by the next function from the 404 handler) then it will render the page-not-found page.
+    console.log(`${err}:`, err.statusCode);
     res.render("page-not-found");
   } else {
+    // If the error's status code is anything other than 404 then it will render the "error" page and log the error message along with the corresponding status code.
     err.message = "Something went wrong on the server" || err.message;
     err.statusCode = err.statusCode || 500;
-    res.status = err.statusCode;
+    res.status(err.statusCode);
     console.log(`${err.message}:`, err.statusCode);
     res.render("error", { err });
   }
